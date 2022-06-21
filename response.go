27052e639d30be3cdd4ac559824a85ed29ba5f0c
Andrei Korcsak <korcsak@tehama.io>
2022-06-21 15:37:49 -0400

    Removed Unnecessary responses

--- response.go
+++ response.go
@@ -1,64 +1,11 @@
 package filter
 
 import (
-	"net"
-
 	"github.com/miekg/dns"
 )
 
 func createReply(r *dns.Msg, ttl uint32) *dns.Msg {
-	// state := request.Request{Req: r}
-
-	// switch state.QType() {
-	// case dns.TypeA:
-	// 	return newNXDomainResponse(r, ttl)
-
-	// case dns.TypeAAAA:
-	// 	return newNXDomainResponse(r, ttl)
-
-	// default:
 	return newNXDomainResponse(r, ttl)
-	// }
-}
-
-func newAResponse(r *dns.Msg, ttl uint32) *dns.Msg {
-	a := new(dns.A)
-	a.Hdr = dns.RR_Header{
-		Name:   r.Question[0].Name,
-		Rrtype: dns.TypeA,
-		Class:  dns.ClassINET,
-		Ttl:    ttl,
-	}
-	a.A = net.IPv4zero
-
-	msg := new(dns.Msg)
-	msg.SetReply(r)
-	msg.SetRcode(r, dns.RcodeSuccess)
-	msg.Answer = []dns.RR{a}
-	msg.Authoritative = true
-	msg.RecursionAvailable = true
-	msg.Compress = true
-	return msg
-}
-
-func newAAAAResponse(r *dns.Msg, ttl uint32) *dns.Msg {
-	aaaa := new(dns.AAAA)
-	aaaa.Hdr = dns.RR_Header{
-		Name:   r.Question[0].Name,
-		Rrtype: dns.TypeAAAA,
-		Class:  dns.ClassINET,
-		Ttl:    ttl,
-	}
-	aaaa.AAAA = net.IPv6zero
-
-	msg := new(dns.Msg)
-	msg.SetReply(r)
-	msg.SetRcode(r, dns.RcodeSuccess)
-	msg.Answer = []dns.RR{aaaa}
-	msg.Authoritative = true
-	msg.RecursionAvailable = true
-	msg.Compress = true
-	return msg
 }
 
 func newNXDomainResponse(r *dns.Msg, ttl uint32) *dns.Msg {
